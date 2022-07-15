54539f15c907be702dc87a934f1de14f3d4a3342
Toty-01 <caucheanthony@gmail.com>
2022-07-15 17:38:15 +0200

    langages: débuts du 2eme chapitre

---
+++ script.js
@@ -0,0 +1,9 @@
+// TYPED
+var typed = new Typed('.typed', {
+  strings: ["Passionné par l univers du web depuis quelques années, j'aime à coder de manière structurée et de façon résponsive.", 
+  "Mes forces : <br>La création de sites web et d'applications en m'aidant de <strong>langages de programmation</strong> divers et variés", 
+  "La recette d'un site réussi : <br>Satisfaire les besoins client et laisser libre cours à <br>la <strong>créativité</strong> et à <br><strong>l'imagination</strong> pour créer quelque chose d'unique"],
+  typeSpeed: 50,
+  backspeed: 20,
+  loop: true
+});
